620b33092b2b43375b21e75a14afc4b0cbadd30c
Mingkang Pan <mingkang.pan@gmail.com>
2019-05-19 19:35:06 +0200

    initial code

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm") version "1.3.31" apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+}
---
+++ repl/src/main/kotlin/com/minki/repl/Main.kt
@@ -0,0 +1,30 @@
+object Write {
+    val words = listOf("Write").toMutableList()
+
+    override fun toString() = words.joinToString(" ")
+}
+
+object like {
+    override fun toString() = "like"
+}
+
+object poem {
+    override fun toString() = "poem"
+}
+
+
+infix fun Write.code(like : like) : Write{
+    words.add("code")
+    words.add(like.toString())
+    return this
+}
+
+infix fun Write.a(poem : poem) : Write{
+    words.add("a")
+    words.add(poem.toString())
+    return this
+}
+
+fun main() {
+    println(Write code like a poem)
+}
