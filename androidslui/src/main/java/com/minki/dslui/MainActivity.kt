5fab1209d40d5aa1735d9958eff37d05ff58cccb
Andrey Mischenko <andrey.mischenko@bandlab.com>
2019-05-20 17:32:02 +0800

    Fix buill file

--- androidslui/src/main/java/com/minki/dslui/MainActivity.kt
+++ androidslui/src/main/java/com/minki/dslui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.minki.dslui
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
