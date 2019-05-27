0bcff74731c5b2c9e9848adfe8ecf6f8f1bab75a
Mingkang Pan <mingkang.pan@willhaben.at>
2019-05-27 15:42:39 +0200

    add test to ktor module and change compile to implementation

--- ktorserver/build.gradle.kts
+++ ktorserver/build.gradle.kts
@@ -21,10 +21,11 @@ repositories {
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    compile("io.ktor:ktor-server-tomcat:$ktor_version")
-    compile("ch.qos.logback:logback-classic:$logback_version")
-    compile("io.ktor:ktor-server-core:$ktor_version")
-    compile("io.ktor:ktor-jackson:$ktor_version")
-    compile("io.ktor:ktor-auth:$ktor_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    implementation("io.ktor:ktor-server-tomcat:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
---
+++ ktorserver/src/test/kotlin/com/minki/ktor/ApplicationTest.kt
@@ -0,0 +1,20 @@
+package com.minki.ktor
+
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
